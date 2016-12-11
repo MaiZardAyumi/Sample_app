53fa0146b3a1d1bbfa364a6bab571cbf13de999f
LiuQi <MaiZardAyumi@gmail.com>
2016-12-11 15:04:05 +0800

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,27 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+
   fixtures :all
   include ApplicationHelper  
-  # Add more helper methods to be used by all tests here...
-  
+    
+  #如果用户已登录，返回true
   def is_logged_in?
     !session[:user_id].nil?
   end
   
-  
+  # 登入指定的用户
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end    
+
+class ActionDispatch::IntegrationTest
+  
+  # 登入指定的用户
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me }}
+  end
+end
