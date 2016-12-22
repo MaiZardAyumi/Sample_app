9d1ed6efef2c1120d2872ec64d0fb4e803be4f99
LiuQi <MaiZardAyumi@gmail.com>
2016-12-22 18:23:01 +0800

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'mufrfy-8080-xyzern.box.myide.io'
+  host = 'mufrfy-8080-rqjpsc.box.myide.io' 
   config.action_mailer.default_url_options = { host: host, protocol: 'https'}
 
   config.action_mailer.perform_caching = false
